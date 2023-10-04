4cb5c9f30389f0c9c5f51e8020c3dca8469e9e32
MayconFvf <142280713+MayconFvf@users.noreply.github.com>
2023-10-04 12:21:18 -0300

    Add files via upload

---
+++ app01/app01/Form1.Designer.cs
@@ -0,0 +1,184 @@
+﻿namespace app01
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.comboBox2 = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.directorySearcher1 = new System.DirectoryServices.DirectorySearcher();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.AllowDrop = true;
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "indica",
+            "sativa",
+            "hybrida",
+            "cbd"});
+            this.comboBox1.Location = new System.Drawing.Point(219, 190);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 21);
+            this.comboBox1.TabIndex = 0;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // comboBox2
+            // 
+            this.comboBox2.FormattingEnabled = true;
+            this.comboBox2.Location = new System.Drawing.Point(219, 266);
+            this.comboBox2.Name = "comboBox2";
+            this.comboBox2.Size = new System.Drawing.Size(121, 21);
+            this.comboBox2.TabIndex = 1;
+            this.comboBox2.SelectedIndexChanged += new System.EventHandler(this.comboBox2_SelectedIndexChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(12, 189);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(171, 18);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Variabilidade genética";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(12, 265);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(82, 18);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Variedade";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.White;
+            this.panel1.Controls.Add(this.pictureBox2);
+            this.panel1.Location = new System.Drawing.Point(531, 12);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(282, 271);
+            this.panel1.TabIndex = 4;
+            this.panel1.Paint += new System.Windows.Forms.PaintEventHandler(this.panel1_Paint);
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Location = new System.Drawing.Point(20, 14);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(240, 238);
+            this.pictureBox2.TabIndex = 0;
+            this.pictureBox2.TabStop = false;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(12, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(227, 126);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 5;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(294, 112);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(147, 26);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "CLIQUE AQUI PARA SABER\r\nSOBRE ESSA CANNABIS  ->";
+            this.label3.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(383, 303);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(430, 185);
+            this.richTextBox1.TabIndex = 7;
+            this.richTextBox1.Text = "";
+            this.richTextBox1.TextChanged += new System.EventHandler(this.richTextBox1_TextChanged);
+            // 
+            // directorySearcher1
+            // 
+            this.directorySearcher1.ClientTimeout = System.TimeSpan.Parse("-00:00:01");
+            this.directorySearcher1.ServerPageTimeLimit = System.TimeSpan.Parse("-00:00:01");
+            this.directorySearcher1.ServerTimeLimit = System.TimeSpan.Parse("-00:00:01");
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ControlDark;
+            this.ClientSize = new System.Drawing.Size(838, 500);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.comboBox2);
+            this.Controls.Add(this.comboBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.panel1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.ComboBox comboBox2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        protected internal System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.DirectoryServices.DirectorySearcher directorySearcher1;
+    }
+}
+
---
+++ app01/app01/Form1.cs
@@ -0,0 +1,174 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Diagnostics.Eventing.Reader;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+
+namespace app01
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            comboBox2.Text = string.Empty;
+            if (comboBox1.Text == "hybrida")
+            {
+                comboBox2.Items.Clear();
+                comboBox2.Items.Add("gorilaglue");
+                comboBox2.Items.Add("gelato");
+            }
+            else if (comboBox1.Text == "indica")
+            {
+                comboBox2.Items.Clear();
+                comboBox2.Items.Add("northern-lights");
+                comboBox2.Items.Add("purple-punch");
+            }
+            else if (comboBox1.Text == "sativa")
+            {
+                comboBox2.Items.Clear();
+                comboBox2.Items.Add("jack-herer");
+                comboBox2.Items.Add("sourdisel");
+            }
+            else if (comboBox1.Text == "cbd")
+            {
+                comboBox2.Items.Clear();
+                comboBox2.Items.Add("HARLEY-TSU");
+                comboBox2.Items.Add("WHITECBG");
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void comboBox2_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            string[] fotoscannabis = Directory.GetFiles(@"C:\Users\Maycon\Desktop\Aplicativos\.vs\aplicativo cannabis\imagen\");
+            foreach (string img in fotoscannabis)
+            {
+                string[] separador = img.Split('\\');
+                string[] nomeSelecionado = separador[8].Split('.');
+
+                if (comboBox2.Text == nomeSelecionado[0])
+                {
+                    string v = @"C:\Users\Maycon\Desktop\Aplicativos\.vs\aplicativo cannabis\imagen\" + comboBox2.Text + ".jpg";
+                    if (File.Exists(v))
+                    {
+                        pictureBox2.ImageLocation = v;
+                        pictureBox2.SizeMode = PictureBoxSizeMode.StretchImage;
+                    }
+                    else
+                    {
+                        MessageBox.Show("imagen nao encontrada" + v);
+                    }
+                }
+            }
+
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void panel1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void pictureBox2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+            string[] textocanabis = Directory.GetFiles(@"C:\Users\Maycon\Desktop\Aplicativos\.vs\aplicativo cannabis\cbd\");
+            foreach (string txt in textocanabis)
+            {
+                string[] separador1 = txt.Split('\\');
+                string[] nomeSelecionado1 = separador1[8].Split('.');
+
+                if (comboBox2.Text == nomeSelecionado1[0])
+                {
+                    string a = (@"C:\Users\Maycon\Desktop\Aplicativos\.vs\aplicativo cannabis\cbd\" + comboBox2.Text + ".txt");
+                    if (File.Exists(a))
+                        try
+                        {
+                            // Abra o arquivo de texto com o aplicativo padrão associado (por exemplo, o Bloco de Notas)
+                            Process.Start(a);
+                        }
+                        catch (Exception ex)
+                        {
+                            MessageBox.Show("Erro ao abrir o arquivo: " + ex.Message);
+                        }
+                }
+
+            }
+        }
+
+        private void richTextBox1_TextChanged(object sender, EventArgs e)
+        {
+            string[] textocannabis1 = Directory.GetFiles(@"C:\Users\Maycon\Desktop\Aplicativos\.vs\aplicativo cannabis\cbd\");
+            foreach (string txt1 in textocannabis1)
+            {
+                string[] separador2 = txt1.Split('\\');
+                string[] nomeSelecionado2 = separador2[8].Split('.');
+
+                if (comboBox2.Text == nomeSelecionado2[0])
+                {
+                    string b = comboBox2.Text + ".txt";
+                    string caminho = Path.Combine(@"C:\Users\Maycon\Desktop\Aplicativos\.vs\aplicativo cannabis\cbd\", b);
+
+                    if (File.Exists(caminho))
+                    {
+                        try
+                        {
+                            string t = File.ReadAllText(caminho);
+                            richTextBox1.Text = t;
+                        }
+                        catch (Exception ex)
+                        {
+                            MessageBox.Show("Erro ao abrir o arquivo: " + ex.Message);
+                        }
+                    }
+                    else
+                    {
+                        MessageBox.Show("Arquivo de texto não encontrado: " + caminho);
+                    }
+                }
+
+                    }
+
+                }
+                }
+            }
+       
+    
+        
+    
+
